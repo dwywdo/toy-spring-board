01cbdcc0fc951c3419dab09790c5e59c898cbf55
Euiyub Jung <dwywdo@gmail.com>
2020-07-15 22:24:58 +0900

    Setup for Board Application

---
+++ src/main/java/com/dwywdo/toyspringboard/ToySpringBoardApplication.java
@@ -0,0 +1,13 @@
+package com.dwywdo.toyspringboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToySpringBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToySpringBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dwywdo/toyspringboard/ToySpringBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwywdo.toyspringboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToySpringBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
