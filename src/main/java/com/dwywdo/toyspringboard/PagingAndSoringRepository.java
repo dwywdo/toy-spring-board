bea07248818653591e35066f023c4b4c25c684fc
Euiyub Jung <dwywdo@gmail.com>
2020-07-17 19:58:23 +0900

    Add Post + PagingAndSortingRepository & Modify .gitignore

---
+++ src/main/java/com/dwywdo/toyspringboard/PagingAndSoringRepository.java
@@ -0,0 +1,7 @@
+package com.dwywdo.toyspringboard;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface PagingAndSoringRepository<Post, Integer> extends PagingAndSortingRepository<Post, Integer> {
+
+}
