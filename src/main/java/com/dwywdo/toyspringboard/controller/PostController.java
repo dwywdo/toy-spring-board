dbd39cee0dce3408e76998eea6a09e04dadb2d93
Euiyub Jung <dwywdo@gmail.com>
2020-07-21 22:38:12 +0900

    Add Simple Logger

--- src/main/java/com/dwywdo/toyspringboard/controller/PostController.java
+++ src/main/java/com/dwywdo/toyspringboard/controller/PostController.java
@@ -2,6 +2,8 @@ package com.dwywdo.toyspringboard.controller;
 
 import com.dwywdo.toyspringboard.model.Post;
 import com.dwywdo.toyspringboard.service.PostService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
@@ -12,31 +14,36 @@ import java.util.Optional;
 @RequestMapping("/board")
 public class PostController {
 
+    private final Logger logger = LoggerFactory.getLogger(PostController.class);
     private final PostService postService;
-
     public PostController(PostService postService) {
         this.postService = postService;
     }
 
-    // todo. Make use of @RequestBody for getting data with HTTP Request Body
     @PostMapping("/upload")
     public @ResponseBody Post uploadPost(@RequestBody Post newPost){
+        logger.info("uploadPost");
         return postService.uploadPost(newPost);
     }
 
     @GetMapping("/{id}")
     public @ResponseBody Optional<Post> getPost(@PathVariable long id){
+        logger.info("getPost");
         return postService.getPost(id);
-    }
-
-    @DeleteMapping("/{id}")
-    public @ResponseBody String deletePost(@PathVariable long id){
-        return postService.deletePost(id);
     }
 
     @GetMapping("/retrieve/all")
     public @ResponseBody Page<Post> getAllPosts(Pageable pageable){
+        logger.info("getAllPosts");
         return postService.getAllPosts(pageable);
     }
+
+    @DeleteMapping("/{id}")
+    public @ResponseBody String deletePost(@PathVariable long id){
+        logger.info("deletePost");
+        return postService.deletePost(id);
+    }
+
+
 
 }
--- src/main/java/com/dwywdo/toyspringboard/service/PostService.java
+++ src/main/java/com/dwywdo/toyspringboard/service/PostService.java
@@ -1,7 +1,10 @@
 package com.dwywdo.toyspringboard.service;
 
+import com.dwywdo.toyspringboard.controller.PostController;
 import com.dwywdo.toyspringboard.model.Post;
 import com.dwywdo.toyspringboard.repository.PostRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -11,6 +14,7 @@ import java.util.Optional;
 @Service
 public class PostService {
 
+    private final Logger logger = LoggerFactory.getLogger(PostService.class);
     private final PostRepository postRepository;
 
     public PostService(PostRepository postRepository) {
@@ -18,16 +22,22 @@ public class PostService {
     }
 
     public Post uploadPost(Post post){
+        logger.info("uploadPost");
         return postRepository.save(post);
     }
 
+    public Optional<Post> getPost(long id) {
+        logger.info("getPost");
+        return postRepository.findById(id);
+    }
+
     public Page<Post> getAllPosts(Pageable pageable){
+        logger.info("getAllPosts");
         return postRepository.findAll(pageable);
     }
 
-    public Optional<Post> getPost(long id) { return postRepository.findById(id); }
-
     public String deletePost(long id) {
+        logger.info("deletePost");
         postRepository.deleteById(id);
         return "Post was deleted";
     }
